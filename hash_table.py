48714f381efbb6356570794107cf477ebeac3e71
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-06-23 19:02:14 -0700

    Initial Hash Table scaffolding

---
+++ hash_table.py
@@ -0,0 +1,12 @@
+class HashTable():
+    def __init__(self) -> None:
+        self.arr = []
+
+    def insert(self, key, val):
+        raise NotImplementedError
+
+    def remove(self, key):
+        raise NotImplementedError
+
+    def get(self, key):
+        raise NotImplementedError
---
+++ main.py
@@ -0,0 +1,4 @@
+from hash_table import HashTable
+
+dict = HashTable()
+dict.insert('protagonist', 'Star Butterfly')
