0af3ed520b0a50680077d94a7a7b67af0c7323f6
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-06-27 16:14:48 -0700

    In-class demo

--- hash_table.py
+++ hash_table.py
@@ -10,3 +10,20 @@ class HashTable():
 
     def get(self, key):
         raise NotImplementedError
+class HashTable:
+    def __init__(self, size = 10) -> None:
+        self.arr = [[] for _ in range(size)]
+        self.size = size
+
+    def insert(self, key, val):
+        index = hash(key) % self.size
+        print(index)
+        # if self.arr[index] is not None:
+        #     raise KeyError("collision!")
+        self.arr[index].append((key, val))
+
+    def get(self, key):
+        index = hash(key) % self.size
+        for k, v in self.arr[index]:
+            if k == key:
+                return v
--- main.py
+++ main.py
@@ -2,3 +2,16 @@ from hash_table import HashTable
 
 dict = HashTable()
 dict.insert('protagonist', 'Star Butterfly')
+print(dict.arr)
+# dict.insert('protagonist', 'Star Butterfly2')
+# print(dict.arr)
+
+print(dict.get('protagonist'))
+
+all_as = ['a' * i for i in range(10)]
+print(all_as)
+for a in all_as:
+    dict.insert(a, 'some_val')
+
+print(dict.get('protagonist'))
+print(dict.get('aaaa'))
